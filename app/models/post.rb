c1827110019bd2e44426d6fe695c8d8c86f5d6a5
Ilya Lysko <ilya.lysko@gmail.com>
2019-12-07 13:43:36 +0300

    Поправил дз 3 (теперь пост может редактировать не только админ, но и владелец поста).

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: {maximum: 200}
 
   def edit_by?(current_user)
-    current_user == self || current_user&.admin?
+    current_user == :user || current_user&.admin?
   end
 
 end
